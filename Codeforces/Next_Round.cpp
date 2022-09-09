6525bd468797c1cfa0c7c52a712b85382acc9e61
Alvee <35369563+0xMALVEE@users.noreply.github.com>
2022-09-09 14:12:50 +0600

    Create Next_Round.cpp

---
+++ Codeforces/Next_Round.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+#define ll long long
+#define pb push_back
+
+
+int main() {
+    int tc, k;
+    cin >> tc >> k;
+
+    int kth_sc = 0;
+
+    vector<int> arr;
+
+    for (int i = 1; i <= tc; i++) {
+        int c;
+        cin >> c;
+        arr.push_back(c);
+        if (k == i) {
+            kth_sc = c;
+        }
+    }
+
+    int next_round = 0;
+
+    for (int i = 1; i <= tc; i++) {
+        if (arr[i - 1] >= kth_sc && arr[i -1] >= 1) {
+            next_round++;
+        }
+    }
+
+    cout << next_round;
+
+    return 0;
+}
