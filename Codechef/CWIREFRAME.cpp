b9725595e1ae4e999a3f6d38b776497f29b921a0
Alvee <35369563+0xMALVEE@users.noreply.github.com>
2022-09-22 17:07:38 +0600

    B C C F

---
+++ Codechef/CWIREFRAME.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#define ll long long
+#define pb push_back
+#define fr(a,b) for(int i = a; i < b; i++)
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define mod 1000000007
+#define inf (1LL<<60)
+#define all(x) (x).begin(), (x).end()
+#define prDouble(x) cout << fixed << setprecision(10) << x
+#define triplet pair<ll,pair<ll,ll>>
+#define goog(tno) cout << "Case #" << tno <<": "
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define read(x) int x; cin >> x
+using namespace std;
+ 
+void init_code(){
+    fast_io;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+}
+ 
+
+int main() {
+   init_code();
+  	
+  int tc;
+  cin >> tc;
+
+	while(tc--){
+	    int n,m,x;
+	    cin >> n >> m >> x;
+	    
+	    cout << ((n * 2) + (m * 2)) * x << endl;
+	}
+  	return 0;
+}
+ 
---
+++ Codechef/FIT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int tc;
+	cin >> tc;
+	while(tc--){
+	    int x;
+	    cin >> x;
+	    cout << x * 10 << endl;
+	}
+	return 0;
+}
