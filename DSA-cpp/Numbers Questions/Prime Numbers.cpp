95c64a5b5fb108e2244b75ab759e5512ed7ebb4e
Alvee <M.Alvee8141@gmail.com>
2023-03-05 17:48:46 +0600

    binary search

--- DSA-cpp/Numbers Questions/Prime Numbers.cpp
+++ DSA-cpp/Numbers Questions/Prime Numbers.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -7,7 +8,7 @@ int main(){
 
     bool isprime = true;
 
-    for(int i =2; i< n ; i++){
+    for(int i =2; i< sqrt(n) ; i++){
         if(n % i == 0){
             cout << "Non-Prime" << endl;
             isprime = false;
