e15cad7bd44e84071fd0a7da2801859861bb6af6
Alvee <M.Alvee8141@gmail.com>
2023-03-30 02:33:54 +0600

    🐱‍💻solved

--- JS/arithmeticExpressionAS.js
+++ JS/arithmeticExpressionAS.js
@@ -30,3 +30,4 @@ function calculate(expression) {
 console.log(calculate('+ 3 4')); // Output: 7
 console.log(calculate('- 3 * 4 5')); // Output: -17
 console.log(calculate('* + 3 4 5')); // Output: 35
+
